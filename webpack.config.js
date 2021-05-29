92e45d85a9ebb20b509bba8d53cc686b6b210612
steven <stevenke60815@gmail.com>
2021-05-29 14:19:57 +0800

    [add] webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,24 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: './js/[name].js'
   },
+  devServer: {
+    compress: true,
+    port: 3000,
+    stats: {
+      assets: true,
+      cached: false,
+      chunkModules: false,
+      chunkOrigins: false,
+      chunks: false,
+      colors: true,
+      hash: false,
+      modules: false,
+      reasons: false,
+      source: false,
+      version: false,
+      warnings: false
+    }
+  },
   module: {
     rules: [
       {
